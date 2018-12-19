07e815b7eeb5082d4705322f9782b41f5c022cfb
well <516681291@qq.com>
2018-12-19 09:35:54 +0800

    init

---
+++ AnimationTest/AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by 喂！ on 2018/12/19.
+//  Copyright © 2018年 well. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationTest/AnimationTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AnimationTest
+//
+//  Created by 喂！ on 2018/12/19.
+//  Copyright © 2018年 well. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
